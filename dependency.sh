80e2d1f019c51f9413cc6b08eef5bd3256d5f663
Christopher R. Reyes <ChrisReyes@alum.mit.edu>
2021-04-05 08:42:52 -0700

    Added sketch

---
+++ dependency.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+echo "Test";
+
+# TODO: Check if the each requested dependency can be found.
+# TODO: Determine the current operating system
+# TODO: Determine default installer by operating system
+# TODO: Set up wrappers around each installer so that each has an identical signature
+# TODO: Add script option to install missing dependencies using the default installer for the current operating system
